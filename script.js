053e501fe28c6c0f06e719b4756992e49c03cf85
Amr Elzawawy <amr22.elzawawy@ymail.com>
2018-11-30 19:22:45 +0200

    Form Styled. JS Validation on Email, Username and Password Ready. PHP and Database Handling left.

---
+++ script.js
@@ -0,0 +1,25 @@
+function validateForm() {
+    var username_val = document.forms["registration_form"]["Username"].value;
+    var password_val = document.forms["registration_form"]["Password"].value;
+    var email_val = document.forms["registration_form"]["Email"].value;
+    var email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!email_regex.test(email_val.toLowerCase())) {
+        alert("Email Field is incorrect. Please Enter a valid Email address !");
+        return false;
+    }
+    else
+        document.forms["registration_form"]["Email"].value = "";
+    if (username_val == "") {
+        alert("Username Field cannot be empty !");
+        return false;
+    }
+    else
+        document.forms["registration_form"]["Username"].value = "";
+    if (password_val == "") {
+        alert("Password Field cannot be empty !");
+        return false;
+    }
+    else
+        document.forms["registration_form"]["Password"].value = "";
+}
